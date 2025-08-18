9ef7187d44ffabb83b0204058599e8d912b10778
etherpilled | ∞ trading <admin@infinitetrading.io>
2025-08-18 13:24:37 -0400

    Create api.R

---
+++ api.R
@@ -0,0 +1,17 @@
+#API Adapter
+
+library(httr)
+
+itp_api <- function(endpoint, params) {
+  url <- paste0("https://api.infinitetrading.io/", endpoint)
+  
+  # For setBot: send query params, no body
+  if (endpoint == "setBot") {
+    response <- POST(url, query = params, body = "", encode = "raw")
+  } else {
+    response <- GET(url, query = params)
+  }
+  
+  content_text <- content(response, "text", encoding = "UTF-8")
+  cat("Response from API:", content_text, "\n")
+}
