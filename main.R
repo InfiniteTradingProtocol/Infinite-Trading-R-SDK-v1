62a9a3b6c1969241406021d28e93d1712cfc21bf
etherpilled | ∞ trading <admin@infinitetrading.io>
2025-08-22 12:18:56 -0400

    Update main.R

--- main.R
+++ main.R
@@ -1,1 +1,9 @@
 #### Main file to load the Infinite Trading SDK ####
+
+#Set your proper working directoty (the folder where this repository is)
+wd = "C:/GitHub/Infinite-Trading-R-SDK-v1/"
+
+#Load all dependencies.
+source(paste0(wd,"/src/coinbase.R"))
+source(paste0(wd,"/src/api.R"))
+
